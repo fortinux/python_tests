ae8b8c065bf9ddf6af295fc834b11c07ba42e050
fortinux <info@fortinux.com>
2021-05-28 15:20:51 +0200

    Update test_suma.py

--- test_suma.py
+++ test_suma.py
@@ -7,7 +7,7 @@ Date: [2021/05/27]
 
 
 def test_suma():
-    assert sum([1, 1, 2, 3, 5, 8]) == 20, "Debe sumar 18"
+    assert sum([1, 1, 2, 3, 5, 8]) == 20, "Debe sumar 20"
 
 
 if __name__ == "__main__":
